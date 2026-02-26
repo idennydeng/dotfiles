f347c6844f8f13d0ef70d2d57f66ff103effe1c1
dengyi <dengyi@zdns.cn>
2026-02-26 14:53:51 +0800

    feat: 配置 GNU 工具链和 Homebrew 镜像
    - 添加 ls --color 别名
    - 配置阿里云 Homebrew 镜像源
    - 添加 GNU 核心工具路径配置
    - 更新 brew.sh 安装更多 GNU 工具
    - 更新 oh-my-zsh 子模块

--- brew.sh
+++ brew.sh
@@ -8,8 +8,28 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade
 
-# Install `wget`
+# Install GNU core utilities (those that come with macOS are outdated).
+brew install coreutils
+brew install findutils
+brew install moreutils
+brew install gawk
+brew install gnu-indent
+brew install gnu-which
+brew install gnu-sed
+brew install gnu-tar
+brew install diffutils
+brew install binutils
+brew install gnutls
+brew install ed
+brew install grep
+brew install gzip
+brew install screen
+brew install watch
+brew install wdiff
 brew install wget
+brew install less
+brew install unzip
+brew install xz
 
 # Install other useful binaries.
 brew install autojump
